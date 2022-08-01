04fa1c9753bec414cb06d93230085df9dedc074f
TaHaSyedKhan <exploit.any@gmail.com>
2022-08-01 22:53:53 +0530

    expense tracker using react

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,36 @@
 import { useState } from 'react';
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filterChange, setFilterChange] = useState('2020');
 
   const filterChnageHandler = (ExpensesYear) => {
      setFilterChange(ExpensesYear);
-  }
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filterChange;
+  });
+
+ 
 
 
     return (
       <div>
         <Card className="expenses">
-        <ExpensesFilter selected={filterChange} onChangeFilter={filterChnageHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
+         <ExpensesFilter 
+           selected={filterChange} 
+           onChangeFilter={filterChnageHandler}
           />
-        ))}
+          <ExpensesChart expenses={filteredExpenses}/>
+         <ExpensesList items={filteredExpenses}/>
         </Card>
 
-        </div>
+      </div>
     );
 }
 
